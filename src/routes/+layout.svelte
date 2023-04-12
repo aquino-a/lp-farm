a5048ad3b7c2d075a52e6f536b0bf32cf08f5d8e
alex <29205589+aquino-a@users.noreply.github.com>
2023-04-12 14:59:45 +0900

    add twitter logo

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -18,8 +18,21 @@
 		<a href="#">Whitepaper</a>
 		<a href="#">Liquidity Locker</a>
 		<a href="/farms">Farms</a>
-		<a target="_blank" href="https://twitter.com/zkinutoken">Twitter</a>
-		<a target="_blank" href="https://t.me/zkinutoken">Telegram</a>
+		<a class="my-auto" target="_blank" href="https://twitter.com/zkinutoken">
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				viewBox="0 0 24 24"
+				width="16"
+				height="16"
+				class="text-zinc-900"
+			>
+				<path
+					d="M21.73 5.18a8.88 8.88 0 0 1-2.58.7 4.4 4.4 0 0 0 1.92-2.43 8.81 8.81 0 0 1-2.8 1.07 4.38 4.38 0 0 0-7.5 3.98 12.45 12.45 0 0 1-9-4.54 4.37 4.37 0 0 0 1.36 5.81 4.34 4.34 0 0 1-1.96-.59v.06a4.37 4.37 0 0 0 3.48 4.27 4.32 4.32 0 0 1-1.97.08 4.36 4.36 0 0 0 4.07 3.03 8.79 8.79 0 0 1-5.43 1.86 8.86 8.86 0 0 1-1.04-.06 12.46 12.46 0 0 0 6.81 1.99c8.07 0 12.48-6.7 12.48-12.48 0-.19 0-.38-.01-.57a8.91 8.91 0 0 0 2.18-2.28z"
+					fill="currentColor"
+				/>
+			</svg>
+		</a>
+		<a class="my-auto" target="_blank" href="https://t.me/zkinutoken">Telegram</a>
 	</nav>
 </div>
 
