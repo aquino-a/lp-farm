b45a335b9fa517b2c4cbf41db111fd1e807595d2
alex <29205589+aquino-a@users.noreply.github.com>
2023-04-17 16:09:48 +0900

    base staker

--- src/lib/stake.ts
+++ src/lib/stake.ts
@@ -1,5 +1,44 @@
+import type { Contract, ContractRunner } from 'ethers';
+
 export interface Staker {
-    stake(): Promise<void>;
-    withdraw(): Promise<void>;
-    redeem(): Promise<void>;
+	stake(amount: number): Promise<void>;
+	unstake(amount: number): Promise<void>;
+	deposit(amount: number): Promise<void>;
+	withdraw(amount: number): Promise<void>;
+	redeem(amount: number): Promise<void>;
+}
+
+class BaseStaker implements Staker {
+	readonly poolId: number;
+	readonly contract: Contract;
+
+	constructor(poolId: number, contract: Contract) {
+		this.poolId = poolId;
+		this.contract = contract;
+	}
+
+	stake = async (amount: number) => {
+		await this.executeContractFunction('stake', amount);
+	};
+
+	unstake = async (amount: number) => {
+		await this.executeContractFunction('unstake', amount);
+	};
+
+	deposit = async (amount: number) => {
+		await this.executeContractFunction('deposit', amount);
+	};
+
+	withdraw = async (amount: number) => {
+		await this.executeContractFunction('withdraw', amount);
+	};
+
+	redeem = async (amount: number) => {
+		await this.executeContractFunction('redeem', amount);
+	};
+
+	private executeContractFunction = async (name: string, amount: number) => {
+		const tx = await this.contract[name](this.poolId, amount);
+		await tx.wait();
+	};
 }
